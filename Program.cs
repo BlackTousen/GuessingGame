488dd19ff264358a2b743774d40df79bc7d657b8
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-11-16 09:43:15 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Moto!");
+            Guess();
+        }
+        static void Guess()
+        {
+            Console.Write("Guess a number:");
+            string input = Console.ReadLine();
+            int number;
+            if (Int32.TryParse(input, out number))
+            {
+                number = Convert.ToInt32(input);
+                Console.WriteLine($"Your number: {number}");
+            }
+            else
+            {
+                Console.WriteLine("Not a valid number!");
+                Guess();
+            }
+
+        }
+    }
+}
